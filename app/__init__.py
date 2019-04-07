c2f2781e4c2d9a96127a42d41ab20705710e904d
Natalya Pimankova <natalya2487@gmail.com>
2019-04-07 14:05:55 +0300

    login form

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,11 @@ def create_app(config_class=Config):
 
     # bootstrap.init_app(app)
 
-    # from . import previews
-    # app.register_blueprint(previews.bp)
-
     from app import routes
     app.register_blueprint(routes.bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
 
